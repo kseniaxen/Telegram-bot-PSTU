2161e0a1b1cce3ec7ed0f79576d8d6919e34f0a9
Kseniya Serdiuk <serdiuk.xen@gmail.com>
2025-05-12 14:37:28 +0300

    In searching of pattern in data from json file

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import os
 import json
-from pathlib import Path
 from aiogram import Bot, Dispatcher, types, Router
 from aiogram.filters import Command
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
@@ -19,10 +18,10 @@ with open("text.json", "r", encoding="utf-8") as file:
 # Главное меню
 main_menu = ReplyKeyboardMarkup(
     keyboard=[
-        [KeyboardButton(text=TEXTS["menu"]["specialties"]), KeyboardButton(text=TEXTS["menu"]["documents"])],
-        [KeyboardButton(text=TEXTS["menu"]["deadlines"]), KeyboardButton(text=TEXTS["menu"]["site"])],
-        [KeyboardButton(text=TEXTS["menu"]["schedule"]), KeyboardButton(text=TEXTS["menu"]["contacts"])],
-        [KeyboardButton(text=TEXTS["menu"]["operator"])]
+        [KeyboardButton(text=TEXTS["buttons"]["specialties"]), KeyboardButton(text=TEXTS["buttons"]["documents"])],
+        [KeyboardButton(text=TEXTS["buttons"]["deadlines"]), KeyboardButton(text=TEXTS["buttons"]["site"])],
+        [KeyboardButton(text=TEXTS["buttons"]["schedule"]), KeyboardButton(text=TEXTS["buttons"]["contacts"])],
+        [KeyboardButton(text=TEXTS["buttons"]["operator"])]
     ],
     resize_keyboard=True
 )
@@ -30,62 +29,62 @@ main_menu = ReplyKeyboardMarkup(
 # Меню направлений подготовки
 specialties_menu = ReplyKeyboardMarkup(
     keyboard=[
-        [KeyboardButton(text=TEXTS["specialties"]["menu"]["bachelor"]), KeyboardButton(text=TEXTS["specialties"]["menu"]["specialist"])],
-        [KeyboardButton(text=TEXTS["specialties"]["menu"]["master"]), KeyboardButton(text=TEXTS["specialties"]["menu"]["aspirantura"])],
-        [KeyboardButton(text="⬅️ Назад")]
+        [KeyboardButton(text=TEXTS["buttons"]["bachelor"]), KeyboardButton(text=TEXTS["buttons"]["specialist"])],
+        [KeyboardButton(text=TEXTS["buttons"]["master"]), KeyboardButton(text=TEXTS["buttons"]["aspirantura"])],
+        [KeyboardButton(text=TEXTS["buttons"]["back"])]
     ],
     resize_keyboard=True
 )
 
 # Список всех кнопок меню
 all_menu_buttons = [
-    TEXTS["menu"]["specialties"],
-    TEXTS["menu"]["documents"],
-    TEXTS["menu"]["deadlines"],
-    TEXTS["menu"]["site"],
-    TEXTS["menu"]["schedule"],
-    TEXTS["menu"]["contacts"],
-    TEXTS["menu"]["operator"],
-    TEXTS["specialties"]["menu"]["bachelor"],
-    TEXTS["specialties"]["menu"]["specialist"],
-    TEXTS["specialties"]["menu"]["master"],
-    TEXTS["specialties"]["menu"]["aspirantura"],
-    "⬅️ Назад"
+    TEXTS["buttons"]["specialties"],
+    TEXTS["buttons"]["documents"],
+    TEXTS["buttons"]["deadlines"],
+    TEXTS["buttons"]["site"],
+    TEXTS["buttons"]["schedule"],
+    TEXTS["buttons"]["contacts"],
+    TEXTS["buttons"]["operator"],
+    TEXTS["buttons"]["bachelor"],
+    TEXTS["buttons"]["specialist"],
+    TEXTS["buttons"]["master"],
+    TEXTS["buttons"]["aspirantura"],
+    TEXTS["buttons"]["back"]
 ]
 
 #Начало работы
 @router.message(Command("start"))
 async def start(message: types.Message):
-    await message.answer(TEXTS["hello"], reply_markup=main_menu, parse_mode="HTML")
+    await message.answer(TEXTS["start"], reply_markup=main_menu, parse_mode="HTML")
 
 #Список направлений подготовки
-@router.message(lambda msg: msg.text == TEXTS["menu"]["specialties"])
+@router.message(lambda msg: msg.text == TEXTS["buttons"]["specialties"])
 async def show_specialties_menu(message: types.Message):
-    await message.answer(TEXTS["specialties"]["name"], reply_markup=specialties_menu, parse_mode="HTML")
+    await message.answer(TEXTS["specialties"]["title"], reply_markup=specialties_menu, parse_mode="HTML")
 
 #Меню направлений подготовки
 @router.message(lambda msg: msg.text in [
-    TEXTS["specialties"]["menu"]["bachelor"],
-    TEXTS["specialties"]["menu"]["specialist"],
-    TEXTS["specialties"]["menu"]["master"],
-    TEXTS["specialties"]["menu"]["aspirantura"]
+    TEXTS["buttons"]["bachelor"],
+    TEXTS["buttons"]["specialist"],
+    TEXTS["buttons"]["master"],
+    TEXTS["buttons"]["aspirantura"]
 ])
 async def handle_specialties(message: types.Message):
-    info = TEXTS["specialties"]["info_main"]
+    info = TEXTS["specialties"]["extra"]["info_main"]
     match message.text:
-        case text if text == TEXTS["specialties"]["menu"]["bachelor"]:
-            programs = TEXTS["specialties"]["bachelor"]
+        case text if text == TEXTS["buttons"]["bachelor"]:
+            programs = TEXTS["specialties"]["data"]["bachelor"]
             title = "🎓 <b>Программы бакалавриата:</b>"
-        case text if text == TEXTS["specialties"]["menu"]["specialist"]:
-            programs = TEXTS["specialties"]["specialist"]
+        case text if text == TEXTS["buttons"]["specialist"]:
+            programs = TEXTS["specialties"]["data"]["specialist"]
             title = "📚 <b>Программы специалитета:</b>"
-        case text if text == TEXTS["specialties"]["menu"]["master"]:
-            programs = TEXTS["specialties"]["master"]
+        case text if text == TEXTS["buttons"]["master"]:
+            programs = TEXTS["specialties"]["data"]["master"]
             title = "👨‍🎓 <b>Программы магистратуры:</b>"
-        case text if text == TEXTS["specialties"]["menu"]["aspirantura"]:
-            programs = TEXTS["specialties"]["aspirantura"]
+        case text if text == TEXTS["buttons"]["aspirantura"]:
+            programs = TEXTS["specialties"]["data"]["aspirantura"]
             title = "👨‍🔬 <b>Программы аспирантуры:</b>"
-            info = TEXTS["specialties"]["info_asp"]
+            info = TEXTS["specialties"]["extra"]["info_asp"]
         case _:
             await message.answer("Неизвестная команда", reply_markup=specialties_menu)
             return
@@ -104,45 +103,68 @@ async def handle_specialties(message: types.Message):
     )
 
 #Назад
-@router.message(lambda msg: msg.text == "⬅️ Назад")
+@router.message(lambda msg: msg.text == TEXTS["buttons"]["back"])
 async def back_to_main_menu(message: types.Message):
     await start(message)
 
 #График работы
-@router.message(lambda msg: msg.text == TEXTS["menu"]["schedule"])
+@router.message(lambda msg: msg.text == TEXTS["buttons"]["schedule"])
 async def show_schedule(message: types.Message):
-    await message.answer(TEXTS["schedule"], parse_mode="HTML")
+    response = (
+            f"{TEXTS["schedule"]["title"]}\n\n" +
+            "\n".join(f"{schedule}" for schedule in TEXTS["schedule"]["data"]) +
+            f"\n\n{TEXTS["schedule"]["extra"]}"
+    )
+    await message.answer(response, parse_mode="HTML")
 
 #Документы
-@router.message(lambda msg: msg.text == TEXTS["menu"]["documents"])
+@router.message(lambda msg: msg.text == TEXTS["buttons"]["documents"])
 async def show_documents(message: types.Message):
-    await message.answer(TEXTS["documents"], parse_mode="HTML")
+    response = (
+            f"{TEXTS["documents"]["title"]}\n\n" +
+            "\n".join(f"{document}" for document in TEXTS["documents"]["data"]) +
+            f"\n\n{TEXTS["documents"]["extra"]}"
+    )
+    await message.answer(response, parse_mode="HTML")
 
 #Контакты
-@router.message(lambda msg: msg.text == TEXTS["menu"]["contacts"])
+@router.message(lambda msg: msg.text == TEXTS["buttons"]["contacts"])
 async def show_contacts(message: types.Message):
-    await message.answer(TEXTS["contacts"], parse_mode="HTML")
+    response = (
+            f"{TEXTS['contacts']['title']}\n\n" +
+            "\n".join(
+                f"{contact['name']}\n{contact['value']}"
+                for contact in TEXTS["contacts"]["data"]
+            ) +
+            f"\n\n{TEXTS['contacts']['extra']}"
+    )
+    await message.answer(response, parse_mode="HTML")
 
 #Сайт
-@router.message(lambda msg: msg.text == TEXTS["menu"]["site"])
+@router.message(lambda msg: msg.text == TEXTS["buttons"]["site"])
 async def show_website(message: types.Message):
-    await message.answer(TEXTS["site"], parse_mode="HTML")
+    response = (
+            f"{TEXTS["site"]["title"]}\n\n" +
+            "\n".join(f"{site}" for site in TEXTS["site"]["data"]) +
+            f"\n\n{TEXTS["site"]["extra"]}"
+    )
+    await message.answer(response, parse_mode="HTML")
 
 #Сроки приема
-@router.message(lambda msg: msg.text == TEXTS["menu"]["deadlines"])
+@router.message(lambda msg: msg.text == TEXTS["buttons"]["deadlines"])
 async def show_deadlines(message: types.Message):
     deadlines = TEXTS["deadlines"]
-    programs = [f"<b>{item['name']}:</b> {item['dates']}" for item in deadlines["periods"]]
+    programs = [f"<b>{item['name']}:</b> {item['dates']}" for item in deadlines["data"]]
 
     response = (
             f"{deadlines['title']}\n\n" +
             "\n".join(f"• {program}" for program in programs) +
-            f"\n\n{deadlines['info']}"
+            f"\n\n{deadlines['extra']}"
     )
     await message.answer(response, parse_mode="HTML")
 
 #Связь с оператором
-@router.message(lambda msg: msg.text == TEXTS["menu"]["operator"])
+@router.message(lambda msg: msg.text == TEXTS["buttons"]["operator"])
 async def contact_operator(message: types.Message):
     await bot.send_message(
         chat_id=int(os.getenv("OPERATOR_CHAT_ID")),
